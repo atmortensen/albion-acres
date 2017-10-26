7cc0b8adf503d1a3a8b9100a083c2bdaae35da26
Alexander Mortensen <alextmortensen@gmail.com>
2017-10-26 10:59:03 -0600

    Finishes drop down menu.

--- src/NotFound/NotFound.component.js
+++ src/NotFound/NotFound.component.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class NotFound extends Component {
 	render() {
 		return (
-				<div className=".not-found-component">Page not found.</div>
+			<div className="not-found-component">Page not found.</div>
 		)
 	}
 }
--- src/shared/Menu/Menu.component.js
+++ src/shared/Menu/Menu.component.js
@@ -4,13 +4,30 @@ import Wrapper from '../Wrapper/Wrapper.component'
 
 
 export default class Menu extends Component {
+	constructor() {
+		super()
+		this.state = {
+			dropped: false
+		}
+	}
+
+	toggleDrop() {
+		this.setState({ hidden: false, dropped: !this.state.dropped })
+	}
+
 	render() {
+		const menuClasses = `menu-links${ this.state.dropped ? ' dropped' : '' }`
 		return (
 			<div className="menu-component">
 				<div className="menu-bar">
 					<Wrapper>
 						<Link className="logo" to="/"><strong>Albion</strong>Acres</Link>
-						<div className="menu-links">
+						<div onClick={this.toggleDrop.bind(this)} className="hamburger">
+							<div className="bar"></div>
+							<div className="bar"></div>
+							<div className="bar"></div>
+						</div>
+						<div onClick={this.toggleDrop.bind(this)} className={menuClasses}>
 							<NavLink className="menu-link" exact to="/">Home</NavLink>
 							<NavLink className="menu-link" to="/goldens">Our Goldens</NavLink>
 							<NavLink className="menu-link" to="/boers">Our Boers</NavLink>
